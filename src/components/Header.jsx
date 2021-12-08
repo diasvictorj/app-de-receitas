dbdc7b4a89c6b32d7b1051c2b23ad1f3e5dace35
João V. Dias <diasvictorj@gmail.com>
2021-12-08 19:01:35 -0300

    Atualizaçao ate o requisito 12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ name, hideSearch }) {
+  const [searchBar, setBar] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <header>
-      <button type="button" data-testid="profile-top-btn" src={ profileIcon }>
+      <Link to="/perfil">
+      <button type="button" data-testid="profile-top-btn" src={ profileIcon }
+      >
         <img src={ profileIcon } alt="" />
       </button>
-
+      </Link>
       <h1 data-testid="page-title">{ name }</h1>
       {
         !hideSearch && (
-          <button type="button" data-testid="search-top-btn" src={ searchIcon }>
+          <button 
+            type="button" 
+            data-testid="search-top-btn" 
+            src={ searchIcon } 
+            onClick={ () => setBar(!searchBar)}
+          >
             <img src={ searchIcon } alt="" />
           </button>
         )
       }
+      { searchBar && (
+        <label htmlFor='"search-text-input"'>
+          Pesquisar
+          <input 
+            id="search-text-input"
+            type="text"
+            data-testid="search-input"
+            value={ searchValue }
+            onChange={ setSearchValue }
+          />
+        </label>
+      )}
     </header>
   );
 }
