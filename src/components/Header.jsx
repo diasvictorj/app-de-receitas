d8345230c6cc05295dd9e04ae376a9124cf4f077
João V. Dias <diasvictorj@gmail.com>
2021-12-09 17:04:28 -0300

    Requisito 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,48 +3,109 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import requestAPI from '../services/requestAPI';
 
 function Header({ name, hideSearch }) {
   const [searchBar, setBar] = useState(false);
   const [searchValue, setSearchValue] = useState('');
+  const [radioValue, setRadioValue] = useState('');
+
+  const handleClick = () => {
+    if (radioValue === 'first-letter'
+      && searchValue.length > 1) {
+      global.alert('Sua busca deve conter somente 1 (um) caracter');
+      return null;
+    }
+    const defineURL = requestAPI(name, searchValue, radioValue);
+    console.log(defineURL);
+    fetch(defineURL)
+      .then((response) => response.json())
+      .then((e) => console.log(e))
+      .catch((error) => console.log('Deu ruim', error));
+  };
 
   return (
-    <header>
-      <Link to="/perfil">
-        <button
-          type="button"
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-        >
-          <img src={ profileIcon } alt="" />
-        </button>
-      </Link>
-      <h1 data-testid="page-title">{ name }</h1>
-      {
-        !hideSearch && (
+    <div>
+
+      <header>
+        <Link to="/perfil">
           <button
             type="button"
-            data-testid="search-top-btn"
-            src={ searchIcon }
-            onClick={ () => setBar(!searchBar) }
+            data-testid="profile-top-btn"
+            src={ profileIcon }
           >
-            <img src={ searchIcon } alt="" />
+            <img src={ profileIcon } alt="" />
           </button>
-        )
-      }
-      { searchBar && (
-        <label htmlFor='"search-text-input"'>
-          Pesquisar
-          <input
-            id="search-text-input"
-            type="text"
-            data-testid="search-input"
-            value={ searchValue }
-            onChange={ setSearchValue }
-          />
-        </label>
+        </Link>
+        <h1 data-testid="page-title">{name}</h1>
+        {
+          !hideSearch && (
+            <button
+              type="button"
+              data-testid="search-top-btn"
+              src={ searchIcon }
+              onClick={ () => setBar(!searchBar) }
+            >
+              <img src={ searchIcon } alt="" />
+            </button>
+          )
+        }
+      </header>
+      {searchBar && (
+        <div>
+          <label htmlFor='"search-text-input"'>
+            Pesquisar
+            <input
+              id="search-text-input"
+              type="text"
+              data-testid="search-input"
+              value={ searchValue }
+              onChange={ (e) => setSearchValue(e.target.value) }
+            />
+          </label>
+          <label htmlFor="ingredient-radio">
+            Ingredientes
+            <input
+              name="radio-filter"
+              id="ingredient-radio"
+              value="ingredient"
+              onChange={ (e) => setRadioValue(e.target.value) }
+              type="radio"
+              data-testid="ingredient-search-radio"
+            />
+          </label>
+          <label htmlFor="name-radio">
+            Nome
+            <input
+              name="radio-filter"
+              id="name-radio"
+              value="name"
+              onChange={ (e) => setRadioValue(e.target.value) }
+              type="radio"
+              data-testid="name-search-radio"
+            />
+          </label>
+          <label htmlFor="first-letter-radio">
+            Busar pela primeira letra
+            <input
+              name="radio-filter"
+              id="first-letter-radio"
+              value="first-letter"
+              onChange={ (e) => setRadioValue(e.target.value) }
+              type="radio"
+              data-testid="first-letter-search-radio"
+            />
+          </label>
+          <button
+            data-testid="exec-search-btn"
+            type="button"
+            onClick={ () => handleClick() }
+          >
+            Buscar
+          </button>
+        </div>
       )}
-    </header>
+    </div>
   );
 }
 
