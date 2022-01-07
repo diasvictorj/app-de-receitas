9c1f68cd19bbf5e11c6d93f6f23e9efd7f9e7d1a
Ricardo Zabir <ricardozabir@MacBook-Pro-de-Ricardo.local>
2022-01-06 21:53:42 -0300

    Tela de explorar comidas/bebidas

--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function ExplorarComidas({ history }) {
+  const [randomId, setRandomId] = useState('');
+  const requestRandom = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((raw) => raw.json())
+      .then((res) => res.meals[0].idMeal)
+      .then((final) => setRandomId(final));
+  };
   return (
     <div>
       <Header name="Explorar Comidas" hideSearch />
       <MenuInferior history={ history } />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+        onClick={ () => requestRandom() }
+      >
+        Me Surpreenda!
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+      {
+        randomId !== '' && <Redirect to={ `/comidas/${randomId}` } />
+      }
     </div>
   );
 }
