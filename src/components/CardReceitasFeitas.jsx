745ca7e833bffd7f32178ff20946008ab40cfac9
Thiago Lordello <thiagolordello@hotmail.com>
2022-01-07 18:58:43 -0300

    finaliza tela de receitas prontas

--- src/components/CardReceitasFeitas.jsx
+++ src/components/CardReceitasFeitas.jsx
@@ -1,31 +1,82 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Share from '../images/shareIcon.svg';
 
-const CardReceitasFeitas = () => {
+function CardReceitasFeitas({ recipe, index }) {
+  const { image, id, area, tags, doneDate,
+    name, category, alcoholicOrNot, type } = recipe;
+  const [linkCopied, setLinkCopied] = useState(false);
+  const history = useHistory();
+
   return (
     <div>
       <section>
-        <img
-          data-testid={ `${index}-horizontal-image` }
-          src=""
-          alt=""
-        />
+        <button type="button" onClick={ () => history.push(`/${type}s/${id}`) }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ image }
+            alt={ name }
+            width="250px"
+          />
+        </button>
+
       </section>
       <section>
-        <h4 data-testid={ `${index}-horizontal-top-text` }>placeholder</h4>
-        <h1 data-testid={ `${index}-horizontal-name` }>placeholder</h1>
-        <h3 data-testid={ `${index}-horizontal-done-date` }>Feita em: placeholder</h3>
+        <h4 data-testid={ `${index}-horizontal-top-text` }>
+          {
+            type === 'comida' ? `${area} - ${category}` : alcoholicOrNot
+          }
+        </h4>
+        <button type="button" onClick={ () => history.push(`/${type}s/${id}`) }>
+          <h1 data-testid={ `${index}-horizontal-name` }>{ name }</h1>
+
+        </button>
+        <h3 data-testid={ `${index}-horizontal-done-date` }>
+          Feita em:
+          {' '}
+          { doneDate }
+        </h3>
       </section>
       <section>
         <button
           type="button"
           data-testid={ `${index}-horizontal-share-btn` }
+          src={ Share }
+          onClick={ () => {
+            setLinkCopied(true);
+            navigator.clipboard.writeText(`http://localhost:3000/${type}s/${id}`);
+          } }
         >
-          share
+          <img src={ Share } alt="compartilhar" />
         </button>
-        <h2 data-testid={ `${index}-${tagName}-horizontal-tag` }>placeholder</h2>
+        {
+          tags && tags.map((tagName) => (
+            <h4
+              key={ Math.random() }
+              data-testid={ `${index}-${tagName}-horizontal-tag` }
+            >
+              {tagName}
+
+            </h4>
+          ))
+        }
+
+        {
+          linkCopied && <p>Link copiado!</p>
+        }
       </section>
     </div>
   );
+}
+
+CardReceitasFeitas.propTypes = {
+  recipe: PropTypes.objectOf([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.array,
+  ]).isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default CardReceitasFeitas;
--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardReceitasFeitas from '../components/CardReceitasFeitas';
 import Header from '../components/Header';
 
 function ReceitasFeitas() {
+  const getRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const INITIAL_STATE = getRecipes || [];
+  const [doneRecipes, setRecipes] = useState(INITIAL_STATE);
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    switch (filter) {
+    case 'comida':
+      return setRecipes((p) => p.filter((recipe) => recipe.type === 'comida'));
+    case 'bebida':
+      return setRecipes((p) => p.filter((recipe) => recipe.type === 'bebida'));
+    default:
+      return setRecipes(INITIAL_STATE);
+    }
+  }, [filter, setRecipes]);
+
   return (
     <div>
       <Header name="Receitas Feitas" hideSearch />
       <section>
         <button
           type="button"
-          data-testid="filter-by-all-bnt"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilter('') }
         >
           All
         </button>
         <button
           type="button"
-          data-testid="filter-by-food-bnt"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('comida') }
         >
           Food
         </button>
         <button
           type="button"
-          data-testid="filter-by-drink-bnt"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('bebida') }
         >
           Drink
         </button>
+        {
+          doneRecipes && doneRecipes.map((doneRcp, index) => (
+            <CardReceitasFeitas key={ doneRcp.id } recipe={ doneRcp } index={ index } />
+          ))
+        }
       </section>
     </div>
   );
