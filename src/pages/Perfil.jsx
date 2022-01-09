fd6a1f6b73eee8a64855fd537b748b05d5c75036
João V. Dias <diasvictorj@gmail.com>
2022-01-09 14:19:23 -0300

    Tela de Perfil Finalizada

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,48 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
-
-function Perfil({ history }) {
-  const {email} = JSON.parse(localStorage.getItem('user'));
-  console.log(email);
+import { useHistory } from 'react-router-dom';
 
 
-  const receitasFeitasBTN = (<button 
-  type='button'
-  data-testid="profile-done-btn"
-  onClick
->
-  Receitas Feitas
-</button>);
+function Perfil() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
 
-const receitasFavoritasBTN = (<button 
-  type='button'
-  data-testid="profile-favorite-btn"
-  onClick
->
-  Receitas Favoritas
-</button>
-);
-
-const logOutBTN = (<button 
-  type='button'
-  data-testid="profile-logout-btn"
-onClick
->
-  LogOut
-</button>
-);
+  function logOut() {
+    localStorage.clear();
+    history.push("/");
+  }
 
   return (
     <div>
-      <Header name="Perfil" hideSearch />
-      <h3 data-testid="profile-email">{email}</h3>
-    
-    {receitasFeitasBTN}      
-    {receitasFavoritasBTN}
-    {logOutBTN}
-      
-      <MenuInferior history={ history } />
+      <Header name='Perfil' hideSearch />
+      <h3 data-testid='profile-email'>{email}</h3>
+
+      <button
+        type='button'
+        data-testid="profile-done-btn"
+        onClick={() => history.push('/receitas-feitas')}
+      >
+        Receitas Feitas
+      </button>
+
+      <button
+        type='button'
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/receitas-favoritas')}
+      >
+        Receitas Favoritas
+      </button>
+
+      <button
+        type='button'
+        data-testid="profile-logout-btn"
+        onClick={() => logOut()}
+      >
+        Sair
+      </button>
+
+      <MenuInferior history={history} />
     </div>
   );
 }
